1eaff5d4cedc5d3304397fe48daa73a86a411c87
Steven Victoria <stevn_95@hotmail.com>
2025-03-20 20:05:42 -0500

    clase 20 marzo

---
+++ uso_de_python.py
@@ -0,0 +1,75 @@
+'''
+operadores aritmeticos
+20 de marzo
+
+'''
+#operador suma 
+
+print(2+6)
+
+#operador multiplicacion
+x=4
+y=6
+#print(x*y)
+
+#operador division 
+z=12/4
+print(z)
+print(type(z)) #el resultado de la division siempore es flotante
+
+#operador division piso (floor)
+print(10/3) #division tradicional
+print(10//3) #division piso
+print(14.5//3)
+'''
+la division piso siempre devuelve el entero mas proximo hacia abajo
+
+'''
+
+#operador modulo
+print(20/3)
+print(20%3)
+
+#ejercico propuesto 
+print(8//-3)
+print(8/-3)
+
+#operador potencia
+print(x**2)
+
+'''
+operador de compracion  
+
+'''
+#es igual a 
+#print("enca"=="enca")
+#print(3==3.00000000000001)
+
+#es diferente de
+#print("palabra"!= "palabra")
+
+'''
+operadore de asginacion 
+'''
+
+#asignacion simple o de igualdadno definicion 
+#w=5
+#incremento 
+#saldo=200
+#aldo= saldo + 1 
+#saldo_b-=8
+#print(saldo_b)
+
+'''
+operadores logicos 
+and: comprueba si todas las condiciones se cumplen, true , false de lo contrario
+or: comprueba si alguna de las condiciones se cumple, true , false de lo contrario
+not: negar el estado de una variable
+'''
+print(x==4 and y==6 )
+print(X==5 or y==8 )
+
+usuario_logueado=True
+click_logout=True
+print(not usuario_logueado)
+#
